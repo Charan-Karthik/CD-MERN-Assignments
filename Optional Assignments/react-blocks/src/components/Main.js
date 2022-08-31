fa8bd8c08d506bccd7a01fc065cdd9824079942b
Charan Karthik <secharank@gmail.com>
2022-08-31 08:56:57 -0700

    completed

--- Optional Assignments/react-blocks/src/components/Main.js
+++ Optional Assignments/react-blocks/src/components/Main.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import Advertisement from './Advertisement'
+import SubContents from './SubContents'
 
 export class Main extends Component {
     render() {
         return (
-            <div className="mainred"></div>
+            <div className="mainred">
+                <div className="subcontentContainer">
+                    < SubContents />
+                    < SubContents />
+                    < SubContents />
+                </div>
+                <div>
+                    < Advertisement />
+                </div>
+            </div>
         )
     }
 }
---
+++ Optional Assignments/react-blocks/src/components/SubContents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class SubContents extends Component {
+    render() {
+        return (
+            <div className="subcontent"></div>
+        )
+    }
+}
+
+export default SubContents

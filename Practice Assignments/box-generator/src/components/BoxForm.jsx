9a4a927e8ab060d5886d77879f1159f7b54710a5
Charan Karthik <secharank@gmail.com>
2022-08-31 14:02:14 -0700

    requirements completed

--- Practice Assignments/box-generator/src/components/BoxForm.jsx
+++ Practice Assignments/box-generator/src/components/BoxForm.jsx
@@ -1,26 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const BoxForm = (props) => {
     const [color, setColor] = useState("")
-    const [allBoxes, setAllBoxes] = useState([])
+    const [allBoxes, setAllBoxes] = useState(['Red', 'Blue', 'Purple'])
 
     const createBox = (e) => {
         e.preventDefault();
-        // const newBox = {color}
         console.log("new box with", color, "color");
+        setAllBoxes([...allBoxes, color]);
         setColor("");
     }
 
     return (
         <div>
-            {/* <form className="form" onSubmit={createBox}>
+            <form className="form" onSubmit={createBox}>
                 <label>Color:</label>
                 <input type="text" onChange={(e) => setColor(e.target.value)} value={color} />
-                <input type="submit" value="Add Box"/>
-            </form> */}
+                <input type="submit" value="Add Box" />
+            </form>
             <br />
             <hr />
-            <div>All boxes: {allBoxes}</div>
+            <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
+                { allBoxes.map( (bob, i) => <div key={i} style={{width:'250px', height:'250px', backgroundColor:bob, marginTop:'10px'}}></div> )}
+            </div>
         </div>
     )
 }

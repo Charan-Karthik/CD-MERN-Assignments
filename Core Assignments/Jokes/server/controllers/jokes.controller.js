5bca16065bb77dd08678216e19716b4b33845858
Charan Karthik <secharank@gmail.com>
2022-09-08 13:09:07 -0700

    routes and functions built and working

--- Core Assignments/Jokes/server/controllers/jokes.controller.js
+++ Core Assignments/Jokes/server/controllers/jokes.controller.js
@@ -2,7 +2,35 @@ const Joke = require('../models/jokes.model');
 
 module.exports.findAllJokes = (req, res) => {
     Joke.find()
-        .then(allJokes => res.json({jokes: allJokes}))
+        .then(allJokes => res.json({ jokes: allJokes }))
         // .then(console.log(res))
-        .catch(err => res.json({message: 'Something went wrong', error: err}))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }))
+}
+
+module.exports.findOneJoke = (req, res) => {
+    Joke.findOne({ _id: req.params._id })
+        .then(oneJoke => res.json({ joke: oneJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }))
+}
+
+module.exports.createJoke = (req, res) => {
+    Joke.create(req.body)
+        .then(createdJoke => res.json({ joke: createdJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }))
+}
+
+module.exports.updateJoke = (req, res) => {
+    Joke.findOneAndUpdate(
+        { _id: req.params._id },
+        req.body,
+        { new: true, runValidators: true }
+    )
+        .then(updatedJoke => res.join({ joke: updatedJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }))
+}
+
+module.exports.deleteJoke = (req, res) => {
+    Joke.deleteOne({ _id: req.params._id })
+        .then(result => res.json({ result: result }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }))
 }
--- Core Assignments/Jokes/server/routes/jokes.routes.js
+++ Core Assignments/Jokes/server/routes/jokes.routes.js
@@ -2,9 +2,9 @@ const JokeController = require('../controllers/jokes.controller')
 
 module.exports = app => {
     app.get('/api/jokes', JokeController.findAllJokes) // return list of all jokes
-    // app.get('api/jokes/:_id') // return one joke with matching id
-    // app.get('api/jokes/random') // return a random joke
-    // app.post('/api/jokes/new') // create a new joke
-    // app.put('/api/jokes/update/:_id') // update a joke with matching id
-    // app.delete('/api/jokes/delete/:_id') // remove joke with matching id
+    app.get('/api/jokes/:_id', JokeController.findOneJoke) // return one joke with matching id
+    // app.get('/api/jokes/random') // return a random joke
+    app.post('/api/jokes/new', JokeController.createJoke) // create a new joke
+    app.put('/api/jokes/update/:_id', JokeController.updateJoke) // update a joke with matching id
+    app.delete('/api/jokes/delete/:_id', JokeController.deleteJoke) // remove joke with matching id
 }
